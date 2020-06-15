8552d8027f684fcbc5adb322efc8f50c4b98c6b9
noCountJun <ansejjang@gmail.com>
2020-06-15 16:20:13 +0900

    first Commit

---
+++ src/com/anse/ex01/Invitation.java
@@ -0,0 +1,7 @@
+package com.anse.ex01;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+	private LocalDateTime when;
+}
---
+++ src/com/anse/ex01/TicketSeller.java
@@ -0,0 +1,25 @@
+package com.anse.ex01;
+
+public class TicketSeller {
+	private TicketOffice ticketOffice;
+	
+	public TicketSeller(TicketOffice ticketOffice) {
+		this.ticketOffice = ticketOffice;
+	}
+	
+	public TicketOffice getTicketOffice() {
+		return ticketOffice;
+	}
+	
+	public void sellTo(Audience audience) {
+		if(audience.getBag().hasInvitation()) {
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().minusAmount(ticket.getFee());
+		} else {
+			Ticket ticket = ticketOffice.getTicket();
+			audience.getBag().minusAmount(ticket.getFee());
+			ticketOffice.plusAmount(ticket.getFee());
+			audience.getBag().setTicket(ticket);
+		}
+	}
+}
